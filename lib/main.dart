3b1aef268758472cc561fd133bb2e12afd2b22c6
tttuuu1234 <yidongyi8@gmail.com>
2024-01-12 21:37:05 +0900

    環境とビルドモードを判別できるように画面に表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,6 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const flavor = String.fromEnvironment('flavor');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -49,6 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text('flavor: $flavor'),
+            const Text('release build: $kReleaseMode'),
             const Text(
               'You have pushed the button this many times:',
             ),
